d115f8709250fb9a0686638141701b66a045846a
FamALouiz <fam@awadlouis.com>
2024-07-20 15:38:36 +0300

    Added genre endpoints and resources

--- movies/app.py
+++ movies/app.py
@@ -8,12 +8,18 @@ movies_ns = Namespace('movies', description='Movies related operations')
 movie_ns = Namespace('movie', description='Movie related operations')
 
 movie_model = movie_ns.model('Movie', {
+    'id': fields.Integer(readOnly=True, description='The movie unique identifier'),
     'title': fields.String(required=True, description='The movie title'),
     'description': fields.String(required=True, description='The movie description'),
     'video_url': fields.String(required=True, description='The movie video url'),
     'thumbnail_url': fields.String(required=True, description='The movie thumbnail url'),
     'duration_in_minutes': fields.Integer(required=True, description='The movie duration in minutes'),
     'genre': fields.String(required=True, description='The movie genre', attribute='genre.name'),
+})
+
+genre_model = movies_ns.model('Genre', {
+    'id': fields.Integer(readOnly=True, description='The genre unique identifier'),
+    'name': fields.String(required=True, description='The genre name')
 })
 
 @movies_ns.route('/')
@@ -23,7 +29,7 @@ class MoviesResource(Resource):
     def get(self):
         return Movie.query.all(), 200
     
-    @movies_ns.expect(movie_model)
+    @movies_ns.expect(movie_model, validate=True)
     @movies_ns.marshal_with(movie_model)
     def post(self):
         data = request.get_json()
@@ -43,6 +49,29 @@ class MoviesResource(Resource):
             return movie, 201
         except Exception as e: 
             return {'message': str(e)}, 404
+    
+@movies_ns.route("/genres/")
+class GenresResource(Resource): 
+        
+        @movies_ns.marshal_list_with(genre_model)
+        def get(self): 
+            genres = Genre.query.all()
+            return genres, 200
+        
+        @movies_ns.expect(genre_model, validate=True)
+        @movies_ns.marshal_with(genre_model)
+        def post(self): 
+            data = request.get_json()
+            genre_name = data['name']
+            
+            try: 
+                genre = Genre(name=genre_name)
+                genre.save()
+                
+                return genre, 201
+            except Exception as e: 
+                return {'message': str(e)}, 404
+
     
 @movie_ns.route('/<int:id>')
 class MovieResource(Resource): 
