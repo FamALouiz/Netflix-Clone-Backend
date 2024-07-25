68113adddc67b967a485de4062560483828194ad
FamALouiz <fam@awadlouis.com>
2024-07-25 19:19:56 +0300

    Returning profile id when profile is queried

--- profiles/app.py
+++ profiles/app.py
@@ -4,6 +4,7 @@ from flask_restx import Namespace, Resource, fields
 profile_ns = Namespace('profiles', description='Profile related operations')
 
 profiles_model = profile_ns.model('Profiles', {
+    'id': fields.Integer(readOnly=True, description='The profile identifier'),
     'first_name': fields.String(required=True, description='The profile first name'),
     'last_name': fields.String(required=True, description='The profile last name'),
 })
