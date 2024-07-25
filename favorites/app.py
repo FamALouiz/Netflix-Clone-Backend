89766e0d8d6fb7da2c02c9f2a705157476f3f06b
FamALouiz <fam@awadlouis.com>
2024-07-25 17:09:28 +0300

    Added favorite endpoint for single favorite movie querying

--- favorites/app.py
+++ favorites/app.py
@@ -4,6 +4,7 @@ from models import User
 from movies.models import Movie
 
 favorites_ns = Namespace('favorites', description='Favorites related operations')
+favorite_ns = Namespace('favorite', description='Favorite related operations')
 
 @favorites_ns.route('/<int:user_id>')
 class FavoritesResource(Resource):
@@ -31,3 +32,28 @@ class FavoritesResource(Resource):
         user.update(favorite_to_be_added=movie_id)
         
         return {'message': 'Favorites updated'}, 200
+
+@favorite_ns.route('/<int:user_id>')
+class FavoriteResource(Resource): 
+    
+    def get(self, user_id):
+        
+        movie_id = request.args.get('movieId')
+        
+        if movie_id is None: 
+            return {'message': 'movieId is required'}, 400        
+                    
+        # Check if user is valid
+        user = User.query.get_or_404(user_id)
+        
+        # Check if movie is valid 
+        movie = Movie.query.get(movie_id)
+
+        if movie is None:
+            return {'message': 'Invalid movieId'}, 400
+        
+        if int(movie_id) in [favorite.movie_id for favorite in user.favorites]:
+            return {"status": True}, 200 
+        else:
+            return {"status": False}, 200
+        
